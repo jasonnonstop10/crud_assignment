6f0161c5ab531f70a6b9acce45b51da9dc5183ad
jasonnonstop10 <jay44411@gmail.com>
2021-06-10 12:34:47 +0700

    [add] curd update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectToDatabase = require("./utils/db");
+const movie = require("./routes/movieRoutes")
 const app = express();
 const port = 3000;
 
@@ -11,6 +12,8 @@ const connectMongo = async (req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(connectMongo);
+app.use(movie());
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const MovieController = require('../controllers/movieController');
+
+router.get('/movie',MovieController.showAll);
+router.get('/movie/:id',MovieController.showByID);
+router.post('/movie/add',MovieController.add);
+router.put('/movie/edit',MovieController.edit);
+router.delete('/movie/delete',MovieController.delete);
+
+module.exports = router
